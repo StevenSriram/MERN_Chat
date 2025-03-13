91e1033e16346ed2b76a12d8ae79277ce0a61e80
StevenSriram <sriram242004@gmail.com>
2025-03-13 23:58:53 +0530

    feat : User Login & Logout

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,21 @@
 import express from "express";
-
+import cookieParser from "cookie-parser";
 import { configEnv } from "../config.js";
 configEnv();
 
 import connectDB from "./db/configDB.js";
 
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-// * Testing Route
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// * Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// * Routes
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   connectDB();
---
+++ Backend/controllers/auth.controller.js
@@ -0,0 +1,100 @@
+import User from "../modals/user.modal.js";
+import bcrypt from "bcrypt";
+
+import generateJWT from "../utils/generateJWT.js";
+
+export const signUp = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // ? Check if user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // ? Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    let user = new User({ name, email, password: hashedPassword });
+
+    // ! Save user
+    await user.save();
+
+    // ? Generate JWT
+    const token = generateJWT(res, user);
+
+    user = { ...user._doc, password: undefined };
+    return res.status(201).json({
+      sucess: true,
+      token,
+      user,
+      message: "User Created Successfully",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ sucess: false, message: "Something went wrong" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // ? Check if user exists
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+
+    // ? Check if password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // ? Generate JWT
+    const token = generateJWT(res, user);
+
+    user = { ...user._doc, password: undefined };
+    return res
+      .status(200)
+      .json({ sucess: true, token, user, message: "Logged In Successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ sucess: false, message: "Something went wrong" });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    // ? Clear JWT cookie
+    res.clearCookie("jwt");
+
+    return res.status(200).json({ sucess: true, message: "Logout successful" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ sucess: false, message: "Something went wrong" });
+  }
+};
+
+export const checkAuth = async (req, res) => {
+  try {
+    const user = req.user;
+
+    return res
+      .status(200)
+      .json({ sucess: true, user, message: "User is authenticated" });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
