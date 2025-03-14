8bdb40847d52f6bc7976407b7946688182933277
StevenSriram <sriram242004@gmail.com>
2025-03-14 22:51:12 +0530

    feat : Node Cache

--- Backend/controllers/message.controller.js
+++ Backend/controllers/message.controller.js
@@ -1,13 +1,26 @@
 import User from "../modals/user.modal.js";
 import Message from "../modals/message.modal.js";
 
+import memoryCache, { generateCacheKey } from "../utils/nodeCache.js";
+
 export const getUsers = async (req, res) => {
   const { id } = req.params;
 
   try {
+    // ! Check cache for users
+    const cacheKey = generateCacheKey("users", id);
+    if (memoryCache.has(cacheKey)) {
+      const userCache = memoryCache.get(cacheKey);
+      return res.status(200).json({ sucess: true, users: userCache });
+    }
+
+    // ? Fetch users except cur User
     const users = await User.find({
       _id: { $ne: id },
     }).select("-password");
+
+    // ! Set cache for users
+    memoryCache.set(`users-${id}`, users);
 
     return res.status(200).json({ sucess: true, users });
   } catch (error) {
@@ -19,6 +32,21 @@ export const getMessages = async (req, res) => {
   const { from, to } = req.params;
 
   try {
+    const fromToKey = generateCacheKey("messages", `${from}-${to}`);
+    const toFromKey = generateCacheKey("messages", `${to}-${from}`);
+
+    // ! Check cache for either one of the keys
+    if (memoryCache.has(fromToKey)) {
+      const cachedMessages = JSON.parse(memoryCache.get(fromToKey));
+      return res.status(200).json({ success: true, messages: cachedMessages });
+    }
+
+    if (memoryCache.has(toFromKey)) {
+      const cachedMessages = JSON.parse(memoryCache.get(toFromKey));
+      return res.status(200).json({ success: true, messages: cachedMessages });
+    }
+
+    // ? Fetch messages
     const messages = await Message.find({
       $or: [
         { sender: from, receiver: to },
@@ -26,7 +54,11 @@ export const getMessages = async (req, res) => {
       ],
     });
 
-    return res.status(200).json({ sucess: true, messages });
+    // ! Set caches
+    memoryCache.set(fromToKey, JSON.stringify(messages));
+    memoryCache.set(toFromKey, JSON.stringify(messages));
+
+    return res.status(200).json({ success: true, messages });
   } catch (error) {
     return res.status(500).json({ message: "Something went wrong" });
   }
@@ -51,7 +83,14 @@ export const sendMessage = async (req, res) => {
     // ! Save message
     await message.save();
 
-    return res.status(200).json({ sucess: true, message });
+    const fromToKey = generateCacheKey("messages", `${from}-${to}`);
+    const toFromKey = generateCacheKey("messages", `${to}-${from}`);
+
+    // ! Clear the cache for both keys
+    memoryCache.del(fromToKey);
+    memoryCache.del(toFromKey);
+
+    return res.status(200).json({ success: true, message });
   } catch (error) {
     return res.status(500).json({ message: "Something went wrong" });
   }
